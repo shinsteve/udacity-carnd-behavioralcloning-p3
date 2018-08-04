30c243cbb1e100356f2aa439b6ed195a985242b0
shinsteve <39026765+shinsteve@users.noreply.github.com>
2018-08-04 15:50:55 +0900

    Upload final model and result
    - Implemented network model similar to NVIDIA's work
    - Add video.mp4 that demonstrates vehicle driving autonomously
    - Add graphs that illustrates the history of learning

--- model.py
+++ model.py
@@ -1,37 +1,49 @@
-import csv
+import sys
 import os
+import csv
 import cv2
 import numpy as np
 import sklearn
+from keras.models import Sequential
+from keras.layers import Convolution2D, Activation, Dropout, MaxPooling2D, Flatten, Dense, Cropping2D, Lambda
 
+TAG = ''
 SHOW_HISTORY = False
 
+# Param for data augmentation
 GEN_FLIP_IMG = True
 USE_MULTICAM_IMG = True
 SIDE_CAM_ANGLE_CORRECTION = 0.2
 
+# Param for image preprocessing
 ENABLE_CROP = True
 CROP_TB = (50, 20)
 CROP_LR = (0, 0)
-ENABLE_GRAYSCALE = True
+ENABLE_GRAYSCALE = False
 
-BATCH_SIZE = 2
+# Param for Dropout layer
+DROP_RATE_CONV = 0.2
+DROP_RATE_DENSE = 0.4
 
-# dataset_path = '{}/behavioral_cloning/driving_dataset'.format(os.getenv('UDACITY_DATASET_PATH'))
-dataset_path = '{}/behavioral_cloning/dummy'.format(os.getenv('UDACITY_DATASET_PATH'))
+BATCH_SIZE = 64
+NUM_EPOCH = 12
+
+dataset_path = '{}/behavioral_cloning/driving_dataset'.format(os.getenv('UDACITY_DATASET_PATH'))
+# dataset_path = '{}/behavioral_cloning/dummy'.format(os.getenv('UDACITY_DATASET_PATH'))
 
 
 def main():
     train_samples, validation_samples = load_dataset()
     train_generator = generator(train_samples, batch_size=BATCH_SIZE)
     validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
-    train_model(train_generator, validation_generator,
-                calc_generated_num(train_samples), calc_generated_num(validation_samples))
+    history = train_model(train_generator, validation_generator,
+                          calc_generated_num(train_samples), calc_generated_num(validation_samples))
+    show_history(history)
     return
 
 
 def load_dataset():
-    """ Return tuple of list of sample data information """
+    """ Return tuple of list of sample data information (not image)"""
     samples = []
     with open(os.path.join(dataset_path, 'driving_log.csv')) as csvfile:
         reader = csv.reader(csvfile)
@@ -47,7 +59,7 @@ def calc_generated_num(samples):
         num_data_with_multicam = sum(int(has_multicam_image(data)) for data in samples)
         num += 2 * num_data_with_multicam  # adding left and right image data
     if GEN_FLIP_IMG:
-        num *= 2
+        num *= 2  # Original + Flippled
     return num
 
 
@@ -68,9 +80,9 @@ def generator(samples, batch_size):
                 img_path = img_path.strip()
                 filename = img_path.split('\\')[-1]
                 current_path = os.path.join(os.path.join(dataset_path, 'IMG'), filename)
-                print(current_path)
+                # print(current_path)
                 image = cv2.imread(current_path)  # BGR order
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Needed for drive.py for inferrence
                 images.append(image)
                 angles.append(angle)
                 return
@@ -97,17 +109,18 @@ def generator(samples, batch_size):
 
 
 def train_model(train_generator, validation_generator, num_train_samples, num_validation_samples):
-    from keras.models import Sequential
-    from keras.layers import Flatten, Dense, Cropping2D, Lambda
-
     print('num train: ', num_train_samples)
     print('num validation: ', num_validation_samples)
+
     model = Sequential()
     ch = 3
+    shape = (160, 320, ch)
+
+    # Preprocessing image
     if ENABLE_CROP:
-        model.add(Cropping2D(cropping=(CROP_TB, CROP_LR), input_shape=(160, 320, ch)))
-    shape = (160 - (CROP_TB[0] + CROP_TB[1]), 320 - (CROP_LR[0] + CROP_LR[1]), ch)
-    # Grayscaling
+        model.add(Cropping2D(cropping=(CROP_TB, CROP_LR), input_shape=shape))
+        shape = (shape[0] - (CROP_TB[0] + CROP_TB[1]), shape[1] - (CROP_LR[0] + CROP_LR[1]), ch)
+
     if ENABLE_GRAYSCALE:
         # model.add(Lambda(lambda x: tf.image.rgb_to_grayscale(x)))  ## cannot save to pickel
         def rgb2gray(x):  # Stackoverflow #46836358
@@ -119,23 +132,53 @@ def train_model(train_generator, validation_generator, num_train_samples, num_va
     # Normalize and 0 mean
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=shape, output_shape=shape))
 
-    model.add(Flatten(input_shape=shape))
-    model.add(Dense(1))
+    model = build_network(model)
 
     model.compile(loss='mse', optimizer='adam')
     history = model.fit_generator(train_generator,
                                   samples_per_epoch=num_train_samples,
                                   validation_data=validation_generator,
                                   nb_val_samples=num_validation_samples,
-                                  nb_epoch=7)
-    if SHOW_HISTORY:
-        show_history(history)
+                                  nb_epoch=NUM_EPOCH)
 
-    model.save('model.h5')
-    return
+    model.save('model_{}.h5'.format(TAG))
+    return history
+
+
+def build_network(model):
+    # This network is based on NVIDIA's work
+    model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Dropout(DROP_RATE_CONV))
+
+    model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Dropout(DROP_RATE_CONV))
+
+    model.add(Convolution2D(48, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Dropout(DROP_RATE_CONV))
+
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Dropout(DROP_RATE_CONV))
+
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Dropout(DROP_RATE_CONV))
+
+    model.add(Flatten())
+
+    model.add(Dense(100))
+    model.add(Dropout(DROP_RATE_DENSE))
+    model.add(Dense(50))
+    model.add(Dropout(DROP_RATE_DENSE))
+    model.add(Dense(16))
+    # model.add(Dropout(DROP_RATE_DENSE))
+    model.add(Dense(1))
+
+    return model
 
 
 def show_history(history_object):
+    import matplotlib
+    if not SHOW_HISTORY:
+        matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     # print the keys contained in the history object
     print(history_object.history.keys())
@@ -146,8 +189,13 @@ def show_history(history_object):
     plt.ylabel('mean squared error loss')
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
-    plt.show()
+    if SHOW_HISTORY:
+        plt.show()
+    else:
+        plt.savefig('history_{}.png'.format(TAG))
     return
 
 if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        TAG = sys.argv[1]
     main()
